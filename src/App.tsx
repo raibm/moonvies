af9861e73041bd31a8c77a5bb37180764effa084
rai.maciel <rai.maciel@basis.com.br>
2020-09-17 21:20:50 -0300

    Remocao de coisa inutil

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import GlobalStyle from './styles/GlobalStyles';
-import MainGrid from './components/MainGrid';
 import LoginPage from './components/Login';
 
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <>
     <LoginPage/>
-    {/* <MainGrid/> */}
     <GlobalStyle/>
     </>
   );
